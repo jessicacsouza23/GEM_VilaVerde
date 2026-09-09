15a950232ac7840fd8634a19626df12f4fe61078
jessicacsouza23 <jessicacsouza23@gmail.com>
2026-09-09 13:01:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -506,6 +506,7 @@ DIF_PRATICA = ["Não estudou nada", "Estudou de forma insatisfatória", "Não as
                "Esquece de colocar o pé direito no pedal de expressão", "Faz movimentos desnecessários com o pé esquerdo na pedaleira", 
                "Dificuldade com o uso do metrônomo", "Estuda sem o metrônomo", "Dificuldades em ler as notas na clave de sol", 
                "Dificuldades no movimento da mão", "Demonstra insegurança ao lidar com o conteúdo",
+               "Dificuldade em leitura ritmica", "Dificuldades em leitura métrica",
                "Dificuldades em ler as notas na clave de fá", "Não realizou as atividades da apostila", "Dificuldade em fazer a articulação ligada e semiligada",
                "Dificuldade com as respirações", "Dificuldade com as respirações sobre passagem", 
                "Dificuldades em recurso de dedilhado (passagem, alargamento, contração, mudança ou substituição)", "Dificuldade em fazer nota de apoio", 
